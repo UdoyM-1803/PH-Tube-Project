252afda540dfaf61c141e52c74cd0a1699908fe8
UdoyM-1803 <udoymunna17@gmail.com>
2025-11-05 01:51:16 +0600

    Create button from fetched data and hovered them

--- script/index.js
+++ script/index.js
@@ -1,14 +1,31 @@
 console.log("Index is connected.");
 
 function loadCategories() {
-    // We will fetch the Data
+    // 1. We will fetch the Data
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+
+    // 2. Convert promise to json()
         .then((res) => res.json())
+        // 3. Send the data to display.
         .then((data) => displayCategories(data.categories));
 }
 
 function displayCategories(categories) {
-    console.log(categories);
+    // Get the container
+    const categoryContainer =  document.getElementById("category-container");
+
+    // Loop Operation on Array of Object
+    for (let cat of categories) {
+        console.log(cat);
+        // Create Element
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
+            <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+        // Append Element
+        categoryContainer.append(categoryDiv);
+    }
+        
 }
 
 loadCategories();
